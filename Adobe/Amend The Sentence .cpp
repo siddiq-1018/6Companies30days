2ea90109d0dcf648c3a123748577c2ee858a5e1d
siddiq-1018 <siddiqparveen333@gmail.com>
2022-01-20 20:45:51 +0530

    Adobe Coding problems

---
+++ Adobe/Amend The Sentence .cpp
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    string amendSentence (string s)
+    {
+       string ans = "" ;
+       ans += tolower(s[0]) ;
+       int i=1 ;
+       while( s[i] != '\0') {
+           if(isupper(s[i])) ans += " " ;
+           ans += tolower(s[i]) ;
+           i++ ;
+       }
+       return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		string s; cin >> s;
+		Solution ob;
+		cout << ob.amendSentence (s) << endl;
+	}
+}  // } Driver Code Ends
